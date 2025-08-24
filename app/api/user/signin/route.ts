7bf44ed404e4e56cd92cf72d855659e57279bdb0
Deepak Maurya <deepakmry4@gmail.com>
2025-08-24 15:32:50 +0530

    Added some shits

--- app/api/user/signin/route.ts
+++ app/api/user/signin/route.ts
@@ -2,22 +2,20 @@ import { prisma } from "@/prisma-client";
 import { NextRequest, NextResponse } from "next/server";
 import z from "zod";
 
-const OrgSignInSchema = z.object({
+const UserSignInSchema = z.object({
   id: z.string().optional(),
   email: z.string(),
   name: z.string(),
-  description: z.string(),
   city: z.string(),
   state: z.string(),
 });
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const parsedData = z.safeParse(OrgSignInSchema, body);
-
+  const parsedData = z.safeParse(UserSignInSchema, body);
 
   if (parsedData.success) {
-    const newOrg = await prisma.organization.create({ data: parsedData.data });
+    const newOrg = await prisma.user.create({ data: parsedData.data });
     parsedData.data.id = newOrg.id;
     return NextResponse.json(parsedData);
   } else return NextResponse.json(parsedData, { status: 400 });
